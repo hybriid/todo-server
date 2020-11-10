e9d2cd1c9e13b88b07d096b74c51acfdb3d0915e
Matthew <mdeguz@Matthews-MacBook-Pro-2.local>
2020-11-09 19:05:33 -0800

    Add all CRUD operations

--- build.sc
+++ build.sc
@@ -1,16 +1,21 @@
-// build.sc
 import mill._, scalalib._
+
 
 object todoServer extends ScalaModule{
   def scalaVersion = "2.13.2"
+  def ivyDeps = Agg(
+    ivy"io.getquill::quill-jdbc:3.5.2",
+    ivy"org.postgresql:postgresql:42.2.8",
+    ivy"com.opentable.components:otj-pg-embedded:0.13.1",
+    ivy"com.lihaoyi::cask:0.7.4",
+    ivy"com.lihaoyi::scalatags:0.9.1"
+  )
   object test extends Tests{
-    def ivyDeps = Agg(
-      ivy"io.getquill::quill-jdbc:3.5.2",
-      ivy"org.postgresql:postgresql:42.2.8",
-      ivy"com.opentable.components:otj-pg-embedded:0.13.1",
-      ivy"com.lihaoyi::cask:0.7.4",
-      ivy"com.lihaoyi::scalatags:0.9.1"
-    )
     def testFrameworks = Seq("utest.runner.Framework")
+
+    def ivyDeps = Agg(
+      ivy"com.lihaoyi::utest::0.7.4",
+      ivy"com.lihaoyi::requests::0.6.5",
+    )
   }
 }
--- todoServer/src/main.scala
+++ todoServer/src/main.scala
@@ -1,7 +1,60 @@
-package foo
-object Example{
-  def main(args: Array[String]): Unit = {
-    println(hello())
+package todoServer
+import io.getquill._
+import com.zaxxer.hikari.{HikariConfig, HikariDataSource}
+import com.opentable.db.postgres.embedded.EmbeddedPostgres
+
+object main extends cask.MainRoutes {
+  case class Note(id: String, content: String)
+  val server = EmbeddedPostgres.builder()
+    .setDataDirectory(System.getProperty("user.home") + "/data")
+    .setCleanDataDirectory(false).setPort(5432)
+    .start()
+  val pgDataSource = new org.postgresql.ds.PGSimpleDataSource()
+  pgDataSource.setUser("postgres")
+  val hikariConfig = new HikariConfig()
+  hikariConfig.setDataSource(pgDataSource)
+  val ctx = new PostgresJdbcContext(LowerCase, new HikariDataSource(hikariConfig))
+  ctx.executeAction("CREATE TABLE IF NOT EXISTS note (id text, content text);")
+  import ctx._
+
+  def addNoteDatabase(id: String, content: String) = {
+    ctx.run(query[Note].insert(lift(Note(id, content))))
   }
-  def hello(): String = "Hello World"
+
+  def deleteNoteDatabase(id: String) = {
+    ctx.run(query[Note].filter(note => note.id == lift(id)).delete)
+  }
+
+  def updateNoteDatabase(id: String, content: String) {
+    ctx.run(query[Note].filter(note => note.id == lift(id)).update(Note(lift(id), lift(content))))
+  }
+
+  def getAllNotes() = {
+    ctx.run(query[Note].map(n => (n.id, n.content)))
+  }
+
+  @cask.postJson("/add")
+  def addNote(id: String, content: String) = {
+    addNoteDatabase(id, content)
+    getAllNotes()
+  }
+
+  @cask.postJson("/delete")
+  def deleteNote(id: String) = {
+    deleteNoteDatabase(id)
+    getAllNotes()
+  }
+
+  @cask.postJson("/update")
+  def deleteNote(id: String, content: String) = {
+    updateNoteDatabase(id, content)
+    getAllNotes()
+  }
+
+  @cask.post("/do-thing")
+  def doThing(request: cask.Request) = {
+    request.text().reverse
+  }
+
+  initialize()
 }
